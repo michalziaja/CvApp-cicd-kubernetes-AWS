53ec0e394063f47b989c7c0e1f409c57dab61230
michalziaja <michalziaja88@gmail.com>
2024-05-15 13:26:08 +0200

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -9,29 +9,20 @@ resource "aws_instance" "ec2" {
   root_block_device {
     volume_size = 20
   }
-  # provisioner "remote-exec" {
-  # inline = ["echo 'Wait until SSH is ready'"]
-  
-  # connection {
-  #     type        = "ssh"
-  #     host        = aws_instance.ec2.public_ip
-  #     user        = "ubuntu"
-  #     private_key = file(var.private_key_path)
-  #   }
-  # }
   tags = {
     Name = var.instance_name
   }
 } 
 
 resource "null_resource" "sleep" {
+  depends_on = [ aws_instance.ec2 ]
   provisioner "local-exec" {
     command = "sleep 60"  # Sleep for 60 seconds
   }
 }
 
 resource "null_resource" "copy_install_script" {
-  depends_on = [null_resource.sleep]
+  depends_on = [ null_resource.sleep ]
   provisioner "file" {
     source      = "${path.module}/install.sh"
     destination = "/home/ubuntu/install.sh"
