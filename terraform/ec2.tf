6d068c10002a156781082ecad304b55ad148c8c9
michalziaja <michalziaja88@gmail.com>
2024-05-15 10:30:57 +0200

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -6,35 +6,34 @@ resource "aws_instance" "ec2" {
   vpc_security_group_ids = [aws_security_group.security-group.id]
   iam_instance_profile   = aws_iam_instance_profile.instance-profile.name
 
-  # root_block_device {
-  #   volume_size = 20
-  # }
-  provisioner "remote-exec" {
-  inline = ["echo 'Wait until SSH is ready'"]
-  
-  connection {
-      type        = "ssh"
-      host        = aws_instance.ec2.public_ip
-      user        = "ubuntu"
-      private_key = file(var.private_key_path)
-    }
+  root_block_device {
+    volume_size = 20
   }
+  # provisioner "remote-exec" {
+  # inline = ["echo 'Wait until SSH is ready'"]
+  
+  # connection {
+  #     type        = "ssh"
+  #     host        = aws_instance.ec2.public_ip
+  #     user        = "ubuntu"
+  #     private_key = file(var.private_key_path)
+  #   }
+  # }
   tags = {
     Name = var.instance_name
   }
 } 
 
-resource "null_resource" "copy_install_script" {
-  provisioner "file" {
-    source      = "${path.module}/install.sh"
-    destination = "/home/ubuntu/install.sh"
-    connection {
-      type        = "ssh"
-      host        = aws_instance.ec2.public_ip
-      user        = "ubuntu"
-      private_key = file(var.private_key_path)
-    }
-  }
-
-  depends_on = [aws_instance.ec2]
-}
+# resource "null_resource" "copy_install_script" {
+#   depends_on = [aws_instance.ec2]
+#   provisioner "file" {
+#     source      = "${path.module}/install.sh"
+#     destination = "/home/ubuntu/install.sh"
+#     connection {
+#       type        = "ssh"
+#       host        = aws_instance.ec2.public_ip
+#       user        = "ubuntu"
+#       private_key = file(var.private_key_path)
+#     }
+#   }
+# }
