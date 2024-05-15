9d1aca75fa6400490123bfbb653e254b52c0be6d
michalziaja <michalziaja88@gmail.com>
2024-05-15 07:03:27 +0200

    Update install.sh

--- terraform/install.sh
+++ terraform/install.sh
@@ -1,71 +1,50 @@
 #!/bin/bash
 
+
 echo "Start Install Script"
+sudo apt update -y >> /dev/null
 
-# Update system
-sudo apt update -y
-
-# Install Docker 
-sudo apt install docker.io -y
-sudo usermod -aG docker ubuntu
-sudo systemctl enable --now docker 
-
-
-# Install AWS CLI
+echo "Install AWS CLI"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo apt install unzip -y
-unzip awscliv2.zip
+sudo apt install unzip -y >> /dev/null
+unzip awscliv2.zip >> /dev/null
 sudo ./aws/install
-sleep 5
 
+echo "Install Docker"
+sudo apt install docker.io -y >> /dev/null
+sudo usermod -aG docker ubuntu
+sudo systemctl enable --now docker
 
-# # Install Kubectl
+echo "Install Kubectl"
 sudo apt update
 sudo curl -LO "https://dl.k8s.io/release/v1.28.4/bin/linux/amd64/kubectl"
 sudo chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
 kubectl version --client
 
-# # Install eksctl
+echo "Install Eksctl"
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
 
-# Install Terraform
-# wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-# echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-# sudo apt update
-# sudo apt install terraform -y
+echo "Install Argo CD"
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.4.7/manifests/install.yaml
+sudo apt install jq -y
 
-# # Install Trivy
-# sudo apt-get install wget apt-transport-https gnupg lsb-release -y
-# wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
-# echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
-# sudo apt update
-# sudo apt install trivy -y
+echo "Install Helm"
+sudo snap install helm --classic
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
 
-# # Install Argo CD with Kubectl
-# kubectl create namespace argocd
-# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.4.7/manifests/install.yaml
-# sudo apt install jq -y
+echo "Install Prometheus"
+helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace
 
-# # Installing Helm
-# sudo snap install helm --classic
+echo "Install Grafana"
+helm install grafana grafana/grafana --namespace monitoring --create-namespace
 
-# # Adding Helm repositories
-
-# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-# helm repo add grafana https://grafana.github.io/helm-charts
-# helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-# helm repo update
-
-# # Install Prometheus
-# helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace
-
-# # Install Grafana
-# helm install grafana grafana/grafana --namespace monitoring --create-namespace
-
-# # Install ingress-nginx
-# helm install ingress-nginx ingress-nginx/ingress-nginx
+echo "Install ingress-nginx"
+helm install ingress-nginx ingress-nginx/ingress-nginx
 
 echo "Install Script Complete"
