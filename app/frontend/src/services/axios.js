f775ceb448c94de6e73baffd0c1c5d874325fa42
michalziaja <michalziaja88@gmail.com>
2024-05-16 14:01:02 +0200

    Update axios.js

--- app/frontend/src/services/axios.js
+++ app/frontend/src/services/axios.js
@@ -4,7 +4,9 @@ import axios from 'axios'
 
 //const baseURL = 'http://fastapi:8000/api/v1/' //Docker
 
-const baseURL = ('http://'+`${process.env.API_HOST}`+':'+`${process.env.API_PORT}`+'/api/v1/')
+//const baseURL = ('http://'+`${process.env.API_HOST}`+':'+`${process.env.API_PORT}`+'/api/v1/')
+
+const baseURL = ('http://cvapp-api:5000/api/v1/')
 
 //const baseURL = process.env.API_URL
 
