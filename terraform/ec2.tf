b6c9f75da42c69142bb9d53fcff84b400ceff355
michalziaja <michalziaja88@gmail.com>
2024-05-16 23:50:44 +0200

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -5,9 +5,10 @@ resource "aws_instance" "ec2" {
   subnet_id              = aws_subnet.public-subnet.id
   vpc_security_group_ids = [aws_security_group.security-group.id]
   iam_instance_profile   = aws_iam_instance_profile.host-profile.name
+  associate_public_ip_address = true
 
   root_block_device {
-    volume_size = 20
+    volume_size = 8
   }
   tags = {
     Name = var.instance_name
@@ -17,7 +18,7 @@ resource "aws_instance" "ec2" {
 resource "null_resource" "sleep" {
   depends_on = [ aws_instance.ec2 ]
   provisioner "local-exec" {
-    command = "sleep 60"  # Sleep for 60 seconds
+    command = "sleep 30"  
   }
 }
 
