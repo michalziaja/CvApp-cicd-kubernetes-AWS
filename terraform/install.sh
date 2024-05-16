40b35ff6e3b99815265b09f6cdd23ecefa195d02
michalziaja <michalziaja88@gmail.com>
2024-05-17 00:04:35 +0200

    Update install.sh

--- terraform/install.sh
+++ terraform/install.sh
@@ -18,17 +18,17 @@ aws s3api list-buckets
 # sudo usermod -aG docker ubuntu
 # sudo systemctl enable --now docker
 
-echo "Install Kubectl"
-sudo apt update
-sudo curl -LO "https://dl.k8s.io/release/v1.28.4/bin/linux/amd64/kubectl"
-sudo chmod +x kubectl
-sudo mv kubectl /usr/local/bin/
-kubectl version --client
+# echo "Install Kubectl"
+# sudo apt update
+# sudo curl -LO "https://dl.k8s.io/release/v1.28.4/bin/linux/amd64/kubectl"
+# sudo chmod +x kubectl
+# sudo mv kubectl /usr/local/bin/
+# kubectl version --client
 
-echo "Install Eksctl"
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-sudo mv /tmp/eksctl /usr/local/bin
-eksctl version
+# echo "Install Eksctl"
+# curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+# sudo mv /tmp/eksctl /usr/local/bin
+# eksctl version
 
 # echo "Install Argo CD"
 # kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.4.7/manifests/install.yaml
