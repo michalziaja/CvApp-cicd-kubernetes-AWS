51e000cc9c7c3cfb0a5cc88ccf48ac12420ae57a
michalziaja <michalziaja88@gmail.com>
2024-05-17 16:40:23 +0200

    a

--- terraform/install.sh
+++ terraform/install.sh
@@ -10,14 +10,6 @@ sudo apt install unzip -y >> /dev/null
 unzip awscliv2.zip >> /dev/null
 sudo ./aws/install
 
-echo "TEST AWS CLI"
-aws s3api list-buckets
-
-# echo "Install Docker"
-# sudo apt install docker.io -y >> /dev/null
-# sudo usermod -aG docker ubuntu
-# sudo systemctl enable --now docker
-
 echo "Install Kubectl"
 sudo apt update
 sudo curl -LO "https://dl.k8s.io/release/v1.28.4/bin/linux/amd64/kubectl"
@@ -29,26 +21,5 @@ echo "Install Eksctl"
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
-
-# echo "Install Argo CD"
-# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.4.7/manifests/install.yaml
-# sudo apt install jq -y
-
-# echo "Install Helm"
-# sudo snap install helm --classic
-# helm repo add eks https://aws.github.io/eks-charts
-# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-# helm repo add grafana https://grafana.github.io/helm-charts
-# helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-# helm repo update
-
-# echo "Install Prometheus"
-# helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace
-
-# echo "Install Grafana"
-# helm install grafana grafana/grafana --namespace monitoring --create-namespace
-
-# echo "Install ingress-nginx"
-# helm install ingress-nginx ingress-nginx/ingress-nginx
 
 echo "Install Script Complete"
