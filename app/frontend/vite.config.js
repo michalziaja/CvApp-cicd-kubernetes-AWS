67db220d8970cd7974e8bf3cacb68672f6e0eb76
michalziaja <michalziaja88@gmail.com>
2024-05-17 15:45:53 +0200

    up

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -1,23 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react({
+//     include: "**/*.jsx"
+//   })],
+//   base: "/",
+//   // server: {
+//   //   watch: {
+//   //     usePolling: true,
+//   //   },
+//   //   host: '0.0.0.0',
+//   //   port: 3000,
+//   // },
+//   build: {
+//     outDir: 'dist', // katalog wyjściowy dla zbudowanych plików
+//     assetsDir: '.', // katalog zasobów
+//     sourcemap: false, // wyłącz mapy źródłowe (jeśli nie są potrzebne)
+//     port: 3000
+//   }
+// })
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    include: "**/*.jsx"
-  })],
   base: "/",
-  // server: {
-  //   watch: {
-  //     usePolling: true,
-  //   },
-  //   host: '0.0.0.0',
-  //   port: 3000,
-  // },
-  build: {
-    outDir: 'dist', // katalog wyjściowy dla zbudowanych plików
-    assetsDir: '.', // katalog zasobów
-    sourcemap: false, // wyłącz mapy źródłowe (jeśli nie są potrzebne)
-    port: 3000
-  }
-})
+  plugins: [react()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
+});
