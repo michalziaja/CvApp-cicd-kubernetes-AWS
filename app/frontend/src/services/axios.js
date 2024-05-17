b1e4c66d576c47869821c5e2e2b6e059bbc72696
michalziaja <michalziaja88@gmail.com>
2024-05-17 21:26:27 +0200

    Update axios.js

--- app/frontend/src/services/axios.js
+++ app/frontend/src/services/axios.js
@@ -6,7 +6,9 @@ import axios from 'axios'
 
 //const baseURL = ('http://'+`${process.env.API_HOST}`+':'+`${process.env.API_PORT}`+'/api/v1/')
 
-const baseURL =  'http://cvapplication.xyz:5000/api/v1/'
+const baseURL =  'http://cvapp-api-svc:5000/api/v1/'
+
+//sprawdzic const baseURL = 'http://cvapp-api-svc.app.svc.cluster.local:5000/api/v1/'
 
 //const baseURL = $process.env.API_HOST
 
