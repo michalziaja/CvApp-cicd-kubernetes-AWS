4a35ce8dcb6b13de3d92d750cd3f2cf8b0ffc43c
michalziaja <michalziaja88@gmail.com>
2024-05-17 16:27:23 +0200

    a

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -24,10 +24,13 @@
 
 
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react({
+    include: "**/*.jsx"
+  })],
   base: "/",
   plugins: [react()],
   preview: {
