f36a1c8aa44aacf9c886f2019bd02f9c736d2622
michalziaja <michalziaja88@gmail.com>
2024-05-17 12:56:38 +0200

    d

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -6,17 +6,18 @@ export default defineConfig({
   plugins: [react({
     include: "**/*.jsx"
   })],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    host: '0.0.0.0',
-    port: 3000,
-  },
+  base: "/",
+  // server: {
+  //   watch: {
+  //     usePolling: true,
+  //   },
+  //   host: '0.0.0.0',
+  //   port: 3000,
+  // },
   build: {
     outDir: 'dist', // katalog wyjściowy dla zbudowanych plików
     assetsDir: '.', // katalog zasobów
-    //sourcemap: false, // wyłącz mapy źródłowe (jeśli nie są potrzebne)
+    sourcemap: false, // wyłącz mapy źródłowe (jeśli nie są potrzebne)
     port: 3000
   }
 })
