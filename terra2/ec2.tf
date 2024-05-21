b6575dbdddbc3b827cca53d6bd3bbe6331992446
michalziaja <michalziaja88@gmail.com>
2024-05-21 15:19:51 +0200

    Update ec2.tf

--- terra2/ec2.tf
+++ terra2/ec2.tf
@@ -53,19 +53,3 @@ resource "null_resource" "copy_install_script" {
 
 
 
-# #sudo apt  install awscli
-
-# # Install kubectl
-# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-# chmod +x kubectl
-# sudo mv kubectl /usr/local/bin/
-
-# echo "Install Eksctl"
-# curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-# sudo mv /tmp/eksctl /usr/local/bin
-# #eksctl version
-
-# # Configure kubectl
-# #aws eks update-kubeconfig --region eu-central-1 --name cvapp-eks
-# 
- 
