297d5c0e471eb60e8329a9ab6340854216d05f8a
michalziaja <michalziaja88@gmail.com>
2024-05-22 00:57:54 +0200

    Update eks.tf

--- terra2/eks.tf
+++ terra2/eks.tf
@@ -15,20 +15,18 @@ resource "aws_eks_cluster" "eks_cluster" {
 }
 
 
-resource "aws_eks_node_group" "eks_node_group" {
-  depends_on = [ aws_eks_cluster.eks_cluster ]
-  
+resource "aws_eks_node_group" "eks_node_group" {  
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "ng-${var.node_group_name}"
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = aws_subnet.private.*.id
 
   scaling_config {
-    desired_size = 2
-    max_size     = 2
+    desired_size = 1
+    max_size     = 1
     min_size     = 1
   }
-  instance_types = ["t2.small"]
+  instance_types = ["t2.medium"]
 
   tags = {
     Name = "${var.cluster_name}/NodeGroup"
