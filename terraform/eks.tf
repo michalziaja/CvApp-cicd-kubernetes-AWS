3d5afdca19a4d10e8e402e1e34c72491ef004c3f
michalziaja <michalziaja88@gmail.com>
2024-05-28 12:34:20 +0200

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -22,11 +22,11 @@ resource "aws_eks_node_group" "eks_node_group" {
   subnet_ids      = aws_subnet.private.*.id
 
   scaling_config {
-    desired_size = 2
-    max_size     = 2
-    min_size     = 2
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
   }
-  instance_types = ["t2.medium"]
+  instance_types = ["t2.large"]
 
   tags = {
     Name = "${var.cluster_name}/NodeGroup"
