bf8a39771de93a24bb0eae66ddeb1ffc9013d0c5
michalziaja <michalziaja88@gmail.com>
2024-05-29 19:18:02 +0200

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -16,6 +16,7 @@ resource "aws_eks_cluster" "eks_cluster" {
 
 
 resource "aws_eks_node_group" "eks_node_group" {  
+  depends_on = [ aws_route.private_route ]
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "ng-${var.node_group_name}"
   node_role_arn   = aws_iam_role.eks_node_role.arn
